8eacedf0d867b043a65be6862f21183ae0384b1a
raz-mon <razmon@post.bgu.ac.il>
2022-04-11 20:44:08 +0300

    added paused flag to kerneltrap

--- kernel/trap.c
+++ kernel/trap.c
@@ -150,7 +150,7 @@ kerneltrap()
   }
 
   // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2 && myproc() != 0 && myproc()->state == RUNNING)
+  if((which_dev == 2 && myproc() != 0 && myproc()->state == RUNNING) || paused==1)
     yield();
 
   // the yield() may have caused some traps to occur,
